d2432dcf8408ff1d2d063386b075923e33e21774
dianavfreitas <dianavfreitass@gmail.com>
2024-06-14 15:38:06 -0400

    Pushing work

--- fetchMovies.js
+++ fetchMovies.js
@@ -1,31 +1,26 @@
 const axios = require("axios");
 
-const fetch = require('node-fetch');
+const API_KEY = '3338d798e614564912c7dce6e7df7a93'; // Replace with your actual API key
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzM4ZDc5OGU2MTQ1NjQ5MTJjN2RjZTZlN2RmN2E5MyIsInN1YiI6IjY2NmIwNGFiNThjYjkxYzY3ZGE3NGQ5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dbIFZkhBLQcNEp7Kj8LHR4MrV0wajYhJbfBfPmKSM-I'
-  }
-};
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-
-async function fetchMovies(){
+async function fetchMovies(movieName) {
     try {
-        const response = await axios.get('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1')
+        const response = await axios.get(`${BASE_URL}/search/movie`, {
+            params: {
+                api_key: API_KEY,
+                query: movieName,
+                include_adult: false,
+                language: 'en-US',
+                page: 1
+            }
+        });
 
-        if(response.data.products.length > 0){
-            return response.data.products[0];
-        }else{
+        if (response.data.results.length > 0) {
+            return response.data.results[0];
+        } else {
             return null;
         }
-    } catch(error){
+    } catch (error) {
         console.error("Error fetching Movie:", error);
         return null;
     }
--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const { fetchMovies } = require("./fetchMovies");
 const app = express();
 const port = 3000;
 
+const path = require("path");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send(``);
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/search", async (req, res) => {
-  const movieName = req.body.productName;
+  const movieName = req.body.movieName;
   const movieDetails = await fetchMovies(movieName);
   if (movieDetails) {
     res.send(` `);
